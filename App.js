8a2e00d804a53814bcf54c6ee66497c14701bf3b
pragya-sinha-au5 <56334712+pragya-sinha-au5@users.noreply.github.com>
2021-03-02 17:15:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import {ChatEngine} from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from './components/LoginForm';
+import "./App.css";
+
+const projectID = '18d6217f-a1f7-482a-a161-f7718bbf9256';
+
+const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />;
+  
+    return (
+      <ChatEngine
+        height="100vh"
+        projectID={projectID}
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+      />
+    );
+  };
+  
+  
+  export default App;
